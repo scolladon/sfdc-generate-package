4aeb8aa7fc1aff880c14c7cc57bb4ba58b6bc324
Sébastien Colladon <scolladon@salesforce.com>
2017-06-22 17:09:46 +0200

    add Custom Label and refacto
    Create strategy and factory
    Use promise to improve speed
    fix extension cleaning

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
-const xml2js = require('xml2js');
+const fs = require('fs');
 const dirTree = require('directory-tree');
 const metadata = require('./lib/utils/metadata');
 const xmlbuilder = require('xmlbuilder');
-const typeStrategyFactory = require('./lib/factories/type-strategy-factory.js');
+const TypeStrategyFactory = require('./lib/factories/type-strategy-factory.js');
+
+const PACKAGE_XML_FILE_NAME = 'package.xml';
 
 module.exports = (config,logger) => {
 
@@ -14,17 +16,24 @@ module.exports = (config,logger) => {
   // The module return this promise
   // This is where the job is done
   return new Promise((resolve, reject) => {
-    const xml = xmlbuilder.create('Package')
+    const typeStrategyFactory = new TypeStrategyFactory();
+    Promise.all(
+      dirTree(config.src).children.filter(child => metadata[child.name] !== undefined && child.children != undefined )
+      .sort((a,b) => metadata[a.name].xmlName.localeCompare(metadata[b.name].xmlName))
+      .map(elem => typeStrategyFactory.getTypeStrategy(elem).build())
+    ).then(result => {
+      const xml = xmlbuilder.create('Package')
       .att('xmlns', 'http://soap.sforce.com/2006/04/metadata')
-      .dec('1.0', 'UTF-8');  
-    dirTree(config.src).children.filter(child => metadata[child.name] !== undefined && child.children != undefined )
-    .sort((a,b) => metadata[a.name].xmlName.localeCompare(metadata[b.name].xmlName))
-    .forEach( child => {
-      typeStrategyFactory(child).build(xml);
-    });
-    xml.ele('version')
-    .t(config.apiVersion);
-    const xmlContent = xml.end({ pretty: true, indent: '  ', newline: '\n' });
-    resolve(xmlContent);
+      .dec('1.0', 'UTF-8');
+
+      result.forEach(elem => {if(!!elem) xml.importDocument(elem)});
+        //xml.importDocument);
+
+      xml.ele('version')
+      .t(config.apiVersion);
+      const xmlContent = xml.end({ pretty: true, indent: '  ', newline: '\n' });
+      fs.writeFileSync(config.output + '/' + PACKAGE_XML_FILE_NAME, xmlContent);
+      resolve();
+    }).catch(reject);
   });
 };
